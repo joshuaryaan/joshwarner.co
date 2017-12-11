f6533a5078cff019cd50c2c9aee5c79e477ab07e
Josh Warner <joshua.ryan.warner@icloud.com>
2017-12-11 15:31:29 +1300

    Section touch event

--- js/script.js
+++ js/script.js
@@ -1,3 +1,10 @@
-$(document).ready(function () { 
+$(document).ready(function() {
+
+    $("section").on({ 'touchstart' : function(){
+        $(this).find(".overlay").css("opacity", "0.94");
+    } });
+    $("section").on({ 'touchend' : function(){
+        $(this).find(".overlay").css("opacity", "0");
+    } });
     
 });
