c726dd3c67afa9fa769ef7a2cbc7511b95640dd0
Josh Warner <joshua.ryan.warner@icloud.com>
2017-12-13 19:35:50 +1300

    nav a touchend

--- js/script.js
+++ js/script.js
@@ -1,10 +1,17 @@
 $(document).ready(function() {
 
+    //Reveal overlay on touch
     $("section").on({ 'touchstart' : function(){
         $(this).find(".overlay").css("opacity", "0.94");
     } });
     $("section").on({ 'touchend' : function(){
         $(this).find(".overlay").css("opacity", "0");
     } });
+    
+    //Make links work on a single click on mobile
+    $("nav a").on("touchend", function(event) {
+      window.location.href = $(this).attr("href");
+    });
+    
     
 });
