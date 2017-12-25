70f14f32fb08e00c94558ac8dd2f0a56b825d0fa
Josh Warner <joshua.ryan.warner@icloud.com>
2017-12-26 10:57:25 +1300

    Tease first section hover on mobile

--- js/script.js
+++ js/script.js
@@ -1,5 +1,11 @@
 $(document).ready(function() {
 
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 10) {
+            $("section .overlay").removeClass("tease");;    
+        }
+    });
+    
     //Reveal overlay on touch
     $("section").on({ 'touchstart' : function(){
         $(this).find(".overlay").css("opacity", "0.94");
