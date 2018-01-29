e67d86b38a6b545e64db0f04f2eebd144bc76ea0
Josh Warner <joshua.ryan.warner@icloud.com>
2018-01-29 21:12:51 +1300

    Adding final content

--- js/script.js
+++ js/script.js
@@ -1,17 +1,14 @@
 $(document).ready(function() {
-    
     //Tease image overlay for 1sec after page load
     setTimeout(function(){
         $("section .overlay").removeClass("tease");
     }, 1000);
-    
     //Incase timer fails, remove tease after scroll event
     $(window).scroll(function() {
         if ($(this).scrollTop() > 10) {
             $("section .overlay").removeClass("tease");    
         }
     });
-    
     //Reveal overlay on touch
     $("section").on({ 'touchstart' : function(){
         $(this).find(".overlay").css("opacity", "0.94");
@@ -19,23 +16,20 @@ $(document).ready(function() {
     $("section").on({ 'touchend' : function(){
         $(this).find(".overlay").css("opacity", "0");
     } });
-    
-    //Make links work on a single click on mobile
+    //Remove animation to make links work on a single click on mobile
     $("nav a").on("touchend", function(event) {
       window.location.href = $(this).attr("href");
     });
-
     //Hide all sections except the top two
-    $("section").css( "opacity", "0" );
+    $("section, .cta").css( "opacity", "0" );
     $("section:nth-of-type(1)").css( "opacity", "1" ); //Replace this line with 'if element above fold'
     $("section:nth-of-type(2)").css( "opacity", "1" );
-    
     //Fade in on scroll
     //Credit to Anna Larson - https://codepen.io/annalarson/pen/GesqK
     //Edited by Josh Warner
     $(window).scroll( function(){
         /* Check the location of each desired element */
-        $("section").each( function(i){
+        $("section, .cta").each( function(i){
             var middle_of_object = $(this).position().top + $(this).outerHeight()/2;
             var bottom_of_window = $(window).scrollTop() + $(window).height();
             /* If the object is half visible in the window, fade it it */
@@ -44,6 +38,5 @@ $(document).ready(function() {
             }
         }); 
     });
-    // End credit
-    
+    // End credit   
 });
